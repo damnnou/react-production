ab61ffd08ee99bebddcec734fd500fd05cbda1f2
damnnou <skdamn@outlook.com>
2024-01-29 14:18:41 +0300

    create theme and useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { render } from "react-dom";
-import React, { Suspense } from "react";
-import Counter from "./components/Counter";
-import "./index.scss";
+import React, { Suspense, useContext, useState } from "react";
+import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import { Link } from "react-router-dom";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { Theme, ThemeContext } from "./styles/theme/ThemeContext";
+import { useTheme } from "./styles/theme/useTheme";
 
 const App = () => {
+    const { theme, handleToggleTheme } = useTheme();
     return (
-        <div className="app">
+        <div className={"app " + theme}>
+            <button onClick={handleToggleTheme}>TOGGLE THEME</button>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
