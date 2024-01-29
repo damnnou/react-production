6ee64271a220d78c05084bdee75ce98345545bf7
damnnou <skdamn@outlook.com>
2024-01-29 18:31:32 +0300

    feature sliced design restructure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,10 @@ import React, { Suspense, useContext, useState } from "react";
 import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
-import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
-import { Theme, ThemeContext } from "./styles/theme/ThemeContext";
-import { useTheme } from "./styles/theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { useTheme } from "app/providers/ThemeProvider";
+import { classNames } from "helpers/classNames/classNames";
 
 const App = () => {
     const { theme, handleToggleTheme } = useTheme();
@@ -17,8 +16,8 @@ const App = () => {
             <Link to={"/about"}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={"/about"} element={<AboutPageLazy />} />
-                    <Route path={"/"} element={<MainPageLazy />} />
+                    <Route path={"/about"} element={<AboutPage />} />
+                    <Route path={"/"} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
