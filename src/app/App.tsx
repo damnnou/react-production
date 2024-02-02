5c195bb867a07ee334d1e031c0aa508c8986416c
damnnou <skdamn@outlook.com>
2024-02-02 20:48:08 +0300

    configure i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AppRouter from "app/providers/router/ui/AppRouter";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
@@ -6,16 +6,26 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { NavBar } from "widgets/NavBar";
 import { SideBar } from "widgets/SideBar";
+import { useTranslation } from "react-i18next";
+
+const Component = () => {
+    const { t, i18n } = useTranslation();
+
+    return <div>{t("Главная страница")}</div>;
+};
 
 const App = () => {
     const { theme } = useTheme();
     return (
         <div className={classNames("app", {}, [theme])}>
-            <NavBar />
-            <div className="content-page">
-                <SideBar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="Loading...">
+                <NavBar />
+                <Component />
+                <div className="content-page">
+                    <SideBar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -9,3 +9,4 @@ declare module "*.scss" {
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
+declare const __IS_DEV__: boolean;
